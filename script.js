0fdddd63d57afd369d0eb641ae670b68e9813eb7
João Moreira <110946307+Joao-H-Moreira@users.noreply.github.com>
2024-12-02 21:43:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// fazer crud 
+let btn = document.querySelector(".cadastrar")
+let btnEditar;
+let btnExcluir;
+let tabela = document.querySelector(".tabela")
+let id=0;
+
+btn.addEventListener("click", () => coletaDados());
+
+function coletaDados(){
+
+}
+
+function criatabela(){
+    let id = document.querySelector("#adiciona");
+    let tipo = document.querySelector("#addEvento");
+    let data = document.querySelector("#date");
+    let valor = document.querySelector("#valor") ;
+}
+
+btnExcluir.addEventListener("click", () => deletaDados());
+
+function deletaDados(){
+    
+}
